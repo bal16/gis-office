a1401497fee651338b1bf81f0abcd497373f44d2
TerserahEnte <mhamanksamlekom@gmail.com>
2025-03-01 20:14:13 +0700

    small changes on js calculation and pages.

--- public/js/calcDist.js
+++ public/js/calcDist.js
@@ -1,45 +1,43 @@
-const map = L.map(document.createElement("div")).setView([51.505, -0.09], 13);
-// Ask user for current location
+const map = L.map(document.createElement("div")).setView([0, 0], 13);
+
 navigator.geolocation.getCurrentPosition(function (location) {
     const latlng = [location.coords.latitude, location.coords.longitude];
-    console.log("🚀 ~ latlng:", latlng);
     localStorage.setItem("currentLocation", JSON.stringify(latlng));
     L.marker(latlng).addTo(map);
 });
+
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
 function calcDistance(start, end, id) {
-    const control = L.Routing.control({
-        waypoints: [
-            L.latLng(start[0], start[1]), // Starting point (example)
-            L.latLng(end[0], end[1]), // Destination point (example)
-        ],
-        routeWhileDragging: true, // Allow the route to update as you drag the markers
-        createMarker: function () {
-            return null;
-        }, // Don't display markers
-    }).addTo(map);
+    const ELEMENTS = document.getElementsByClassName(id);
 
-    // Event listener to show the distance when the route is found
-    control.on("routesfound", function (e) {
-        const route = e.routes[0]; // Get the first route (in case there are multiple routes)
-        let distance = route.summary.totalDistance; // Distance in meters
-        // const time = route.summary.totalTime; // Time in seconds
+    if (!start) {
+        ELEMENTS[0].innerHTML = "Lokasi tidak ditemukan";
+        ELEMENTS[1].innerHTML = "Lokasi tidak ditemukan";
+    } else {
+        const control = L.Routing.control({
+            waypoints: [L.latLng(start[0], start[1]), L.latLng(end[0], end[1])],
+            routeWhileDragging: false,
+            createMarker: function () {
+                return null;
+            },
+        }).addTo(map);
 
-        distance = (distance / 1000).toFixed(2) + " km"; // Display distance in kilometers
+        control.on("routesfound", function (e) {
+            const route = e.routes[0];
+            let distance = route.summary.totalDistance;
 
-        const ELEMENTS = document.getElementsByClassName(id);
+            distance = (distance / 1000).toFixed(2) + " km";
 
-        setTimeout(function () {
-            const text = "Total road distance: " + distance;
-            ELEMENTS[0].innerHTML = text; // Set inner HTML
-            ELEMENTS[1].innerHTML = text; // Set inner HTML
-        }, 2000);
-
-        map.off();
-        // map.remove();
-    });
+            setTimeout(function () {
+                const text = "Jarak: " + distance;
+                ELEMENTS[0].innerHTML = text;
+                ELEMENTS[1].innerHTML = text;
+            }, 2000);
+        });
+    }
+    map.off();
 }
