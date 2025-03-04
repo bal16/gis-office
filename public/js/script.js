96c69b47c1a230916d81c2075fe0a5debdde1a23
TerserahEnte <mhamanksamlekom@gmail.com>
2025-03-04 14:23:18 +0700

    fix: calculate distance

--- public/js/script.js
+++ public/js/script.js
@@ -14,6 +14,8 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 function calcDistance(start, end, className) {
     if (!start || !end || !className) {
         console.error("Error: Invalid arguments provided to calcDistance");
+        ELEMENTS[0].innerHTML = "Jarak tidak ditemukan.";
+        ELEMENTS[1].innerHTML = "Jarak tidak ditemukan.";
         return;
     }
 
@@ -39,7 +41,7 @@ function calcDistance(start, end, className) {
                 return;
             }
 
-            const text = "Total road distance: " + distance;
+            const text = "Jarak: " + distance;
             ELEMENTS[0].innerHTML = text;
             ELEMENTS[1].innerHTML = text;
 
@@ -156,9 +158,13 @@ const makeTableItemElement = (office) => `
                     >
                         Buka Map
                     </a><br />
-                    <span class="pt-2 distance-${
-                        office.id
-                    }">menghitung jarak...</span>
+                    <span class="flex inline-text pt-2 jarak-${office.id}"><svg class="mr-3 -ml-1 size-5 animate-spin text-black"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                        <path class="opacity-75" fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                        </path>
+                    </svg>Menghitung Jarak...</span></span>
                 </td>
                 <td class="rounded-r-xl p-5">
                     <img
@@ -291,7 +297,7 @@ async function handleAJAX(queryParams) {
         history.pushState({}, "", newUrl);
         AJAX_ELEMENTS[0].innerHTML = desktopList;
         AJAX_ELEMENTS[1].innerHTML = mobileList;
-        AJAX_ELEMENTS[2].innerHTML = `Showing ${data.length} of ${totalData} entriessss`;
+        AJAX_ELEMENTS[2].innerHTML = `Showing ${data.length} of ${totalData} entries`;
         AJAX_ELEMENTS[3].innerHTML = makePaginationElement({
             current: currentPage,
             last: lastPage,
@@ -308,7 +314,7 @@ async function handleAJAX(queryParams) {
                     calcDistance(
                         currentLocation,
                         [office.latitude, office.longitude],
-                        `distance-${office.id}`
+                        `jarak-${office.id}`
                     );
                 }
             });
--- resources/views/components/table/item.blade.php
+++ resources/views/components/table/item.blade.php
@@ -9,7 +9,13 @@
             Buka Map
         </a>
         <br />
-        <span class="pt-2 {{ "jarak-$number" }}">menghitung jarak...</span>
+        <span class="flex inline-text pt-2 {{ "jarak-$number" }}"><svg class="mr-3 -ml-1 size-5 animate-spin text-black"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                        <path class="opacity-75" fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                        </path>
+                    </svg>Menghitung Jarak...</span>
     </td>
     <td class="rounded-r-xl p-5">
         <img src="{{ asset('storage/' . $image) }}" class="rounded-xl h-60 w-60 object-cover" />
