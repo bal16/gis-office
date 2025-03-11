51a168ba19d995ce7a35143d1f97e51e5dd6672d
ballll16 <ahmadhamidbalya@gmail.com>
2025-03-11 16:31:37 +0700

    fix(security): header ui

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -1,8 +1,3 @@
-<header class="flex items-center justify-between px-5 py-2 md:px-20 md:py-5 md:max-w-[1280px] mx-auto bg-gray-50 shadow-sm sticky top-0 z-10">
-    <img src="{{ asset('image/logo.png') }}" alt="" class="h-5 md:h-10" />
-    <div class="font-bold text-xs md:text-sm">
-        <a href="{{ route('filament.admin.auth.register') }}">SIGNUP</a>
-        |
-        <a href="{{ route('filament.admin.auth.login') }}">LOGIN</a>
-    </div>
+<header class="flex items-center justify-center md:justify-between px-5 py-2 md:px-20 md:py-5 md:max-w-[1280px] mx-auto bg-gray-50 shadow-sm sticky top-0 z-10 ">
+    <img src="{{ asset('image/logo.png') }}" alt="" class="h-10 md:h-10 " />
 </header>
